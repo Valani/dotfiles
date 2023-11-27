675098d565ed6fba26e6483b0f56839da1ee14bc
Valani <38432812+Valani@users.noreply.github.com>
2023-11-27 18:22:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-def max_value(coin, n):
+def max_value(coin, n, memo={}):
     if n <= 0:
         return 0
     elif n == 1:
         return coin[0]
     elif n == 2:
         return max(coin[0], coin[1])
+    if n not in memo:
+        take_current = coin[n - 1] + max_value(coin, n - 2)
+        skip_current = max_value(coin, n - 1)
+        memo[n] = max(take_current, skip_current)
 
-    take_current = coin[n - 1] + max_value(coin, n - 2)
-    skip_current = max_value(coin, n - 1)
-
-    return max(take_current, skip_current)
+    return memo[n]  
 
 coin = [ 20, 50, 20, 5, 2 ] 
 n = len(coin)
